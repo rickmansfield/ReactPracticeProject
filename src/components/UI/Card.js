586fee9fc9a341ddeb2e2e42628043a8b9651356
Rick Mansfield <PEACEFULRICK@msn.com>
2022-09-14 11:02:42 -0500

    Adds Console.log and comments for clarity

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,6 +2,7 @@ import classes from './Card.module.css';
 
 function Card(props) {
   // const classes = 'card ' + props.className;
+  console.log('props', props);
 
   return (
     <div className={`${classes.card} ${props.className}`}>
@@ -11,3 +12,4 @@ function Card(props) {
 }
 
 export default Card
+// css modules create custom class names and prevent class name collisions. 
